be00dc98387ef07bfc00861bce1fe43e575ef71f
arunsachan <66840949+arunsachan@users.noreply.github.com>
2023-01-20 22:18:19 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+let string=document.getElementById('evaluatedText');
+let length=document.getElementById('letterCount')
+string.addEventListener('keyup', (e)=>{
+    if(e.key !== 'Escape'){
+
+
+    }
+    let str=string.value;
+    length.innerHTML=str.length;
+    console.log(str.length);
+})
+
